44f6d04dc9526f952efb09fc984aebac448856d4
Koen Van Exem <koen@vanexem.be>
2021-06-11 13:20:36 +0200

    RibbonUI in ExcelDNA

--- src/ExcelDNA/Ribbon.cs
+++ src/ExcelDNA/Ribbon.cs
@@ -5,6 +5,7 @@ using InteropApplication = Microsoft.Office.Interop.Excel.Application;
 namespace ExcelDNA
 {
     using System;
+    using System.Windows.Forms;
     using Allors.Excel.Interop;
     using Application;
     using ExcelDna.Integration;
@@ -12,39 +13,48 @@ namespace ExcelDNA
     [ComVisible(true)]
     public class Ribbon : ExcelRibbon
     {
-        public override string GetCustomUI(string RibbonID)
+        public override string GetCustomUI(string _)
         {
-            var application = ExcelDnaUtil.Application;
-            var serviceLocator = new ServiceLocator();
-            this.Program = new Program(serviceLocator);
-            this.AddIn = new AddIn((InteropApplication)application, this.Program);
-            return RibbonResources.Ribbon;
+            try
+            {
+                var application = ExcelDnaUtil.Application;
+                var serviceLocator = new ServiceLocator();
+                this.Program = new Program(serviceLocator);
+                this.AddIn = new AddIn((InteropApplication)application, this.Program);
+                return RibbonResources.Ribbon;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
         }
+
+        public IRibbonUI RibbonUI { get; private set; }
 
         public AddIn AddIn { get; private set; }
 
         public Program Program { get; private set; }
 
-        public override void OnStartupComplete(ref Array custom)
+        public async void OnLoad(IRibbonUI ribbon)
         {
-            base.OnStartupComplete(ref custom);
+            this.RibbonUI = ribbon;
 
-            ExcelAsyncUtil.Run("WebSnippetAsync", null,
-                async delegate
-                {
-                    await this.Program.OnStart(this.AddIn);
-                });
+            try
+            {
+                await this.Program.OnStart(this.AddIn);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
         }
 
-        public override object LoadImage(string imageId)
-        {
+        public override object LoadImage(string imageId) =>
             // This will return the image resource with the name specified in the image='xxxx' tag
-            return RibbonResources.ResourceManager.GetObject(imageId);
-        }
+            RibbonResources.ResourceManager.GetObject(imageId);
 
-        public void OnButtonPressed(IRibbonControl control)
-        {
-            System.Windows.Forms.MessageBox.Show("Hello!");
-        }
+        public void OnButtonPressed(IRibbonControl control) => System.Windows.Forms.MessageBox.Show("Hello!");
     }
 }
