f75c2d10aa397c762735f721dc50636284133099
Koen Van Exem <koen@vanexem.be>
2024-03-11 14:35:47 +0100

    update Nuke and externals

--- build/Build.cs
+++ build/Build.cs
@@ -1,32 +1,23 @@
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.Xunit;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 using static Nuke.Common.Tools.Xunit.XunitTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-[CheckBuildProjectConfigurations]
-[ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Ci);
+    public static int Main() => Execute<Build>(x => x.Ci);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
     [GitVersion] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
@@ -36,8 +27,8 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
+            DotNetClean(v => v.SetProject(SourceDirectory));
+            ArtifactsDirectory.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
@@ -66,7 +57,7 @@ class Build : NukeBuild
        .DependsOn(Compile)
        .Executes(() =>
        {
-           var assembly = (AbsolutePath)GlobFiles(SourceDirectory, "**/Allors.Excel.Interop.Tests.dll").First();
+           var assembly = SourceDirectory.GlobFiles("**/Allors.Excel.Interop.Tests.dll").First();
 
            Xunit2(v => v
                  .SetFramework("net461")
@@ -79,7 +70,7 @@ class Build : NukeBuild
        .DependsOn(Compile)
        .Executes(() =>
        {
-           var projects = new[] { "Allors.Excel", "Allors.Excel.Headless"};
+           var projects = new[] { "Allors.Excel", "Allors.Excel.Headless" };
 
            foreach (var project in projects)
            {
